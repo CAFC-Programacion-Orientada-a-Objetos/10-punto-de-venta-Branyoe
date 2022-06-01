b2c68db463af4253d60f821f45fdb88d9a476189
branyoe <bhernandez1@ucol.mx>
2022-06-01 13:51:14 -0500

    perf: changes in main.js file

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-import fs from 'fs';
-import Keyboard from './models/Keyboard.js';
-import Menu from './models/Menu.js';
-import Product from './models/Product.js';
 import PuntoDeVenta from './models/PuntoDeVenta.js';
-import VentaItem from './models/SaleItem.js';
+
 
 
 const options = [
